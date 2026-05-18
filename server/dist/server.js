59cdbec939c75909ff78e0d1e953e0a0e8c84ede
Nitin kumar <tomarnitin413@gmail.com>
2026-05-19 04:55:03 +0530

    fix cors

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import authRouter from "./routes/authRoutes.js";
 import productRouter from "./routes/productRoutes.js";
@@ -11,21 +11,13 @@ import addressRouter from "./routes/addressRoutes.js";
 import adminRouter from "./routes/adminRoutes.js";
 import deliveryPartnerRouter from "./routes/deliveryPartnerRoutes.js";
 import { stripeWebhook } from "./controllers/webhooks.js";
-
 const app = express();
-
 app.post("/api/stripe", express.raw({ type: "application/json" }), stripeWebhook);
-
 // Middleware
-app.use(cors({
-    origin: "https://grocery-delivery-app-v2.vercel.app",
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
-
 const port = process.env.PORT || 5000;
-
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req, res) => {
     res.send("Server is Live!");
 });
 app.use("/api/auth", authRouter);
@@ -36,13 +28,11 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/addresses", addressRouter);
 app.use("/api/admin", adminRouter);
 app.use("/api/delivery", deliveryPartnerRouter);
-
 // Error handling
-app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+app.use((error, req, res, next) => {
     console.error(error);
     res.status(500).json({ message: error.message });
 });
-
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
